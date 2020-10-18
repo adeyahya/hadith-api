992ab98401f724eb5f5063f76c6691726c170efe
adeyahya <adeyahyaprasetyo@gmail.com>
2020-10-18 10:44:40 +0700

    add rest

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,87 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+
+mod model;
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use model::Book;
+use qstring::QString;
+use sqlx::PgPool;
+use std::collections::HashMap;
+use std::env;
+
+pub type BookMap = HashMap<String, Book>;
+
+async fn get_books(books: web::Data<BookMap>) -> impl Responder {
+    let result = Book::find_all(books.get_ref()).await;
+    match result {
+        Ok(books) => HttpResponse::Ok().json(books),
+        _ => HttpResponse::BadRequest().body("Error trying to read all todos from database"),
+    }
+}
+
+async fn get_book_by_slug(
+    pool: web::Data<PgPool>,
+    books: web::Data<BookMap>,
+    req: HttpRequest,
+) -> impl Responder {
+    let slug: String = req.match_info().get("slug").unwrap().parse().unwrap();
+    let qs = QString::from(req.query_string());
+    let limit = qs.get("limit").unwrap_or("5");
+    let limit: i32 = limit.parse().unwrap();
+    let offset = qs.get("offset").unwrap_or("0");
+    let offset: i32 = offset.parse().unwrap();
+
+    let result = Book::find_by_slug(pool.get_ref(), &slug, books.get_ref(), offset, limit).await;
+    match result {
+        Ok(books) => HttpResponse::Ok().json(books),
+        Err(e) => HttpResponse::from(e),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    // database pool
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+
+    // fetch all books as in memory cache
+    let mut books: HashMap<String, Book> = HashMap::new();
+    let recs = sqlx::query!(
+        r#"
+            SELECT id, name, slug, available
+                FROM books
+            ORDER BY id
+        "#
+    )
+    .fetch_all(&db_pool)
+    .await
+    .unwrap();
+
+    for rec in recs {
+        books.insert(
+            rec.slug.to_owned(),
+            Book {
+                id: rec.id,
+                name: rec.name,
+                slug: rec.slug,
+                available: rec.available,
+            },
+        );
+    }
+
+    HttpServer::new(move || {
+        App::new()
+            .data(db_pool.clone())
+            .data(books.clone())
+            .service(
+                web::scope("/rest")
+                    .route("/books/{slug}", web::get().to(get_book_by_slug))
+                    .route("/books", web::get().to(get_books)),
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
---
+++ api/src/model.rs
@@ -0,0 +1,153 @@
+use crate::BookMap;
+use actix_web::{error, Error, HttpRequest, HttpResponse, Responder};
+use anyhow::Result;
+use futures::future::{ready, Ready};
+use serde::Serialize;
+use sqlx::{FromRow, PgPool};
+
+#[derive(Serialize, FromRow, Clone)]
+pub struct Book {
+    pub id: i32,
+    pub name: String,
+    pub slug: String,
+    pub available: Option<i32>,
+}
+
+#[derive(Serialize, FromRow)]
+pub struct Hadith {
+    pub number: i32,
+    pub indonesian: String,
+    pub arabic: String,
+    pub book_slug: String,
+    pub book_name: String,
+}
+
+#[derive(Serialize, FromRow)]
+pub struct HadithPagination {
+    pub book: Book,
+    pub items: Vec<Hadith>,
+    pub limit: i32,
+    pub offset: i32,
+    pub total: i32,
+}
+
+impl Responder for Book {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+        // create response and set content type
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
+    }
+}
+
+impl Responder for Hadith {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+        // create response and set content type
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
+    }
+}
+
+impl Responder for HadithPagination {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+        // create response and set content type
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
+    }
+}
+
+impl Book {
+    pub async fn find_all(books: &BookMap) -> Result<Vec<Book>> {
+        let mut books: Vec<Book> = books.values().cloned().collect();
+        books.sort_by(|a, b| a.id.cmp(&b.id));
+        Ok(books)
+    }
+
+    pub async fn find_by_slug(
+        pool: &PgPool,
+        slug: &str,
+        books: &BookMap,
+        offset: i32,
+        limit: i32,
+    ) -> Result<HadithPagination, Error> {
+        match books.get(slug) {
+            Some(val) => {
+                let book = val.clone();
+                let available = match book.available {
+                    Some(val) => val.clone(),
+                    _ => 0,
+                };
+                let mut hadiths: Vec<Hadith> = Vec::new();
+
+                let recs = sqlx::query!(
+                    r#"
+                        SELECT number, indonesian, arabic
+                            FROM hadiths
+                            WHERE book_id = $1
+                        ORDER BY number
+                        LIMIT $2
+                        OFFSET $3
+                    "#,
+                    book.id,
+                    limit as i64,
+                    offset as i64
+                )
+                .fetch_all(pool)
+                .await
+                .unwrap();
+
+                for rec in recs {
+                    hadiths.push(Hadith {
+                        number: rec.number,
+                        indonesian: rec.indonesian,
+                        arabic: rec.arabic,
+                        book_name: book.name.to_owned(),
+                        book_slug: book.slug.to_owned(),
+                    });
+                }
+
+                Ok(HadithPagination {
+                    offset,
+                    limit,
+                    book,
+                    items: hadiths,
+                    total: available,
+                })
+            }
+            None => Err(error::ErrorNotFound("not found")),
+        }
+    }
+}
+
+// impl Hadiths {
+//     pub async paginate(books: &BookMap, pool: &PgPool, slug: &str, offset: i32, limit: i32) -> Result<Hadiths> {
+//        let recs = sqlx::query!(
+//             r#"
+//                 SELECT number, indonesian, arabic
+//                     FROM hadiths
+//                     WHERE book_id = $1
+//                 ORDER BY number
+//                 LIMIT $2
+//                 OFFSET $3
+//             "#,
+//             book_id,
+//             limit,
+//             offset
+//         ).fetch_all(pool)
+//            .await?;
+//     }
+// }
